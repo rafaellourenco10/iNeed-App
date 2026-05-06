a65e5af9170253f0e4861fa4b9dcf6adb76430b9
Rafael <rafaelrodriguestb@gmail.com>
2026-05-05 23:41:39 -0300

    docs: adiciona pasta src

---
+++ src/main.dart
@@ -0,0 +1,6 @@
+// Arquivo principal do aplicativo iNeed (Flutter)
+// O código-fonte oficial será desenvolvido nesta pasta a partir da próxima Sprint.
+
+void main() {
+  print('Iniciando o app iNeed...');
+}
